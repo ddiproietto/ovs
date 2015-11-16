1f5604fd58fc3cb9be6c0515da0e92a8a9f3e295
Daniele Di Proietto <diproiettod@vmware.com>
2015-11-15 22:07:25 -0800

    dpif-netdev: Implement conntrack flush interface.
    New functions are implemented in the conntrack module to support this.
    
    Signed-off-by: Daniele Di Proietto <diproiettod@vmware.com>
    Acked-by: Flavio Leitner <fbl@sysclose.org>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -4281,6 +4281,14 @@ dpif_netdev_ct_dump_done(struct dpif *dpif OVS_UNUSED,
     return err;
 }
 
+static int
+dpif_netdev_ct_flush(struct dpif *dpif, const uint16_t *zone)
+{
+    struct dp_netdev *dp = get_dp_netdev(dpif);
+
+    return conntrack_flush(&dp->conntrack, zone);
+}
+
 const struct dpif_class dpif_netdev_class = {
     "netdev",
     dpif_netdev_init,
@@ -4324,7 +4332,7 @@ const struct dpif_class dpif_netdev_class = {
     dpif_netdev_ct_dump_start,
     dpif_netdev_ct_dump_next,
     dpif_netdev_ct_dump_done,
-    NULL,                       /* ct_flush */
+    dpif_netdev_ct_flush,
 };
 
 static void
